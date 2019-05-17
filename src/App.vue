c760bedb7dccd4952339babfe3581cadc16096f1
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-05-17 23:00:46 +0200

    a nav drawer component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>SLOCK</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
       <router-view></router-view>
     </v-content>
+    <Menu v-model="drawer"/>
   </v-app>
 </template>
 
 <script>
+import Menu from "./components/Menu";
 export default {
   name: "App",
-  components: {},
+  components: { Menu },
   data() {
     return {
-      //
+      drawer: null
     };
   }
 };
 </script>
+
---
+++ src/components/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-navigation-drawer v-bind:value="value" v-on:input="$emit('input', $event)" absolute temporary>
+    <v-list class="pa-1">
+      <v-list-tile avatar>
+        <v-list-tile-avatar>
+          <img src="https://randomuser.me/api/portraits/men/85.jpg">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>John Leider</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+
+      <v-list-tile v-for="item in items" :key="item.title" @click>
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "dashboard" },
+        { title: "About", icon: "question_answer" }
+      ]
+    };
+  }
+};
+</script>
