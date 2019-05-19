8a125bcc7a040f19561fdb98fa6a6efea5a0391d
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-05-19 10:25:06 +0200

    truck title

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <v-toolbar app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>SLOCK</span>
+        <v-layout align-start justify-center row fill-height>
+          <v-flex>
+            <v-img :src="img.imgTruck" width="30"/>
+          </v-flex>
+          <v-flex pl-2>Truckstop</v-flex>
+        </v-layout>
       </v-toolbar-title>
     </v-toolbar>
 
@@ -16,12 +21,16 @@
 
 <script>
 import Menu from "./components/Menu";
+import imgTruck from "./assets/foodtruck.png";
 
 export default {
   name: "App",
   components: { Menu },
   data() {
     return {
+      img: {
+        imgTruck
+      },
       drawer: null
     };
   }
